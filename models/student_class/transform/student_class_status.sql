e9abc481e5a5824c763943d4f0c294c17259dd7b
balanideepanshu92@gmail.com <balanideepanshu92@gmail.com>
2022-01-15 19:16:18 +0000

    some new transofrmations

--- models/student_class/transform/student_class_status.sql
+++ models/student_class/transform/student_class_status.sql
@@ -6,6 +6,7 @@ with student as (
 class as (
     select * from {{ source('DEMO_DB', 'APPLICATION_STATUS') }}
 )
-select S.NAME,S.AGE,S.ADDRESS,S.BIRTHDAY, S.CLASS, S.TEACHER,T.STATUS
- from student S join class T ON S.name = T.name
+select S.NAME,S.AGE,S.ADDRESS, S.BIRTHDAY, to_varchar('2013-04-05'::date, 'mon dd, yyyy') AS BIRTH
+, S.CLASS, S.TEACHER,T.STATUS
+ from student S join class T ON S.name = T.name  ORDER BY S.AGE ASC
 
